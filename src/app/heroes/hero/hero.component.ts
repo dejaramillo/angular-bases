fcc750b152145925bf33618af03d5b3e73d77c38
unknown <dejaramillo953@misena.edu.co>
2023-07-10 11:16:58 -0500

    [ADD] first commit

---
+++ src/app/heroes/hero/hero.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-hero',
+  templateUrl: './hero.component.html',
+  styleUrls: ['./hero.component.css']
+})
+export class HeroComponent {
+
+  public name: string = 'Ironman';
+  public age: number = 45;
+
+  get capitalizeName(): string {
+    return this.name.toUpperCase();
+  }
+
+  getHeroDescription(): string {
+    return `${ this.name } -  ${ this.age }`;
+  }
+
+  changeHero(): void {
+    this.name = 'Spiderman';
+  }
+  changeAge(): void {
+    this.age = 20;
+  }
+
+  resetFrom(): void {
+    // this.name = 'Ironman';
+    this.age = 45;
+    document.querySelectorAll('h1')!.forEach(e => {
+        e.innerHTML = '<h1>Desde Angular</h1>';
+    })
+  }
+
+}
