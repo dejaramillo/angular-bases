c2e7e18a35a873cdc767641be1a4ae6c5555141b
unknown <dejaramillo953@misena.edu.co>
2023-07-10 12:49:54 -0500

    [ADD] dbz module

---
+++ src/app/dbz/dbz.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AddCharacterComponent } from './components/add-character/add-character.component';
+import { ListComponent } from './components/list/list.component';
+import { MainPageComponent } from './pages/main-page.component';
+
+
+
+@NgModule({
+  exports: [
+    MainPageComponent
+  ],
+  declarations: [
+    AddCharacterComponent,
+    ListComponent,
+    MainPageComponent,
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class DbzModule { }
